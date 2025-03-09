3ff33d5182bd5096a9e05b5e1c56b945c5553a0d
Boris Todorov <btodorovtodorov@gmail.com>
2025-03-09 22:35:30 +0200

    update register button

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -7,7 +7,7 @@ export default function Register() {
                 <input type="password" placeholder="Password" name="password" required />
                 <input type="password" placeholder="Repeat Password" name="rePassword" required />
                 <button type="submit" className="register-btn">
-                    Login
+                    Register
                 </button>
             </form>
             {/* <a href="#">Forgot password?</a> */}
