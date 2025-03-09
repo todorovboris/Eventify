19dc29a095ca7db5f9281a82fb25c8d8508d59aa
Boris Todorov <btodorovtodorov@gmail.com>
2025-03-09 22:32:50 +0200

    add login and register pages

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,16 @@
 export default function Login() {
     return (
-        <div className="login-page">
-            <h2>Login Page</h2>
+        <div className="login-container">
+            <h2>Login</h2>
+            <form>
+                <input type="email" placeholder="Email" name="email" required />
+                <input type="password" placeholder="Password" name="password" required />
+                <button type="submit" className="login-btn">
+                    Login
+                </button>
+            </form>
+            {/* <a href="#">Forgot password?</a> */}
+            <a href="/register">Don't have an account? Sign up</a>
         </div>
     );
 }
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,7 +1,17 @@
 export default function Register() {
     return (
-        <div className="register-page">
-            <h2>Register Page</h2>
+        <div className="register-container">
+            <h2>Register</h2>
+            <form>
+                <input type="email" placeholder="Email..." name="email" required />
+                <input type="password" placeholder="Password" name="password" required />
+                <input type="password" placeholder="Repeat Password" name="rePassword" required />
+                <button type="submit" className="register-btn">
+                    Login
+                </button>
+            </form>
+            {/* <a href="#">Forgot password?</a> */}
+            <a href="/login">Already registered? Log in</a>
         </div>
     );
 }
