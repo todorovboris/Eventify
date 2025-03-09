d6564c20b9f5b3d19aa38593a2b43abdae963781
Boris Todorov <btodorovtodorov@gmail.com>
2025-03-09 14:58:42 +0200

    footer update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,19 +1,61 @@
 export default function Footer() {
     return (
-        <footer className="footer">
-            <nav className="footer-nav">
-                <a href="/" className="footer-link">
-                    Home
-                </a>
-                <a href="/events" className="footer-link">
-                    Events
-                </a>
-                <a href="/profile" className="footer-link">
-                    Profile
-                </a>
-            </nav>
+        <footer>
+            <div className="footer-container">
+                <div className="footer-section">
+                    <h3>About us</h3>
+                    {/* <p>Кратко описание на вашата организация.</p> */}
+                    <p>
+                        <a href="/introduce" className="footer-button">
+                            Contact us
+                        </a>
+                    </p>
+                    <p>
+                        <a href="/contact" className="footer-button">
+                            More information
+                        </a>
+                    </p>
+                </div>
+                <div className="footer-section">
+                    <h3>Information</h3>
 
-            <p>&copy; Copyright - Eventify {new Date().getFullYear()} </p>
+                    <p>
+                        <a href="/privacy" className="footer-button">
+                            Privacy policy
+                        </a>
+                    </p>
+                    <p>
+                        <a href="/terms" className="footer-button">
+                            Terms and conditions
+                        </a>
+                    </p>
+                    {/* <li>
+                            <a href="/sitemap" className="footer-button">Карта на сайта</a>
+                        </li> */}
+                </div>
+                <div className="footer-section">
+                    <h3>Contacts</h3>
+                    <p>
+                        Email: <a href="mailto:info@example.com">info@eventify.com</a>
+                    </p>
+                    <p>Phone: +359 123 456 789</p>
+                    <p>Address: "Street" str. 1, Sofia</p>
+                </div>
+                <div className="footer-section">
+                    <h3>Socials</h3>
+                    <div className="social-icons">
+                        <a href="http://facebook.com">
+                            <img src="/images/facebook.svg" alt="Facebook" />
+                        </a>
+                        <a href="http://instagram.com">
+                            <img src="/images/instagram.svg" alt="Instagram" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="copyright">
+                <p>&copy; Copyright - Eventify {new Date().getFullYear()} </p>
+            </div>
         </footer>
     );
 }
