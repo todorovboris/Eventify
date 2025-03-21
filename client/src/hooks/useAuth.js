000cdb8f1c3eea498ddf8e2237df9b9e08c10b96
Boris Todorov <btodorovtodorov@gmail.com>
2025-03-21 17:16:13 +0200

    add useAuth for authenticated request for events

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,3 +1,29 @@
+import { useContext } from 'react';
+import { UserContext } from '../contexts/UserContext.js';
+import request from '../utils/request.js';
+
 export default function useAuth() {
-    //
+    const authData = useContext(UserContext);
+
+    const requestWrapper = (method, url, eventData, options = {}) => {
+        const authOptions = {
+            ...options,
+            headers: {
+                'X-Authorization': authData.accessToken,
+                ...options.headers,
+            },
+        };
+
+        return request.baseRequest(method, url, eventData, authData.accessToken ? authOptions : options);
+    };
+
+    return {
+        ...authData,
+        request: {
+            get: (url, options) => requestWrapper('GET', url, null, options),
+            post: (url, data, options) => requestWrapper('POST', url, data, options),
+            put: (url, data, options) => requestWrapper('PUT', url, data, options),
+            delete: (url, options) => requestWrapper('DELETE', url, null, options),
+        },
+    };
 }
