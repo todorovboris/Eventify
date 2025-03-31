947f702f72325c0bc553a3ba4cb8f1dd98f95717
Boris Todorov <btodorovtodorov@gmail.com>
2025-03-31 19:42:07 +0300

    add search functionality on home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,23 +1,80 @@
 import { Link } from 'react-router';
-import categoryOptions from '../../helpers/categoryOptions.js';
-import { useLatestEvents, useTopEvents } from '../../api/eventsApi.js';
+import { useAllEvents, useLatestEvents, useTopEvents } from '../../api/eventsApi.js';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
+    let [eventsResult, setEventsResult] = useState([]);
+    const [searchInput, setSearchInput] = useState('');
     const { latestEvents } = useLatestEvents();
     const { topEvents } = useTopEvents();
+    const { events } = useAllEvents();
+    const searchResultsRef = useRef(null);
+
+    const eventSearchHandler = (e) => {
+        e.preventDefault();
+
+        const eventDataForSearch = searchInput.toLowerCase().trim();
+
+        const filteredEvents = events.filter(
+            (event) =>
+                event.title.toLowerCase().includes(eventDataForSearch) ||
+                event.description.toLowerCase().includes(eventDataForSearch) ||
+                event.date.toLowerCase().includes(eventDataForSearch) ||
+                event.location.toLowerCase().includes(eventDataForSearch)
+        );
+
+        setEventsResult(filteredEvents);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (searchResultsRef.current && !searchResultsRef.current.contains(event.target)) {
+                setEventsResult([]);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [searchResultsRef]);
 
     return (
         <div className="home-page">
             <section className="hero">
                 <h1>Find it. Join it. Live it.</h1>
                 <p>The best place to create and participate in events.</p>
-                <div className="search-box">
-                    <input type="text" placeholder="Search for events..." className="search-input" />
-                    <button className="search-btn">Search</button>
-                </div>
+                <form className="search-box" onSubmit={eventSearchHandler}>
+                    <input
+                        type="text"
+                        name="searchInput"
+                        placeholder="Search for events..."
+                        className="search-input"
+                        value={searchInput}
+                        onChange={(e) => setSearchInput(e.target.value)}
+                    />
+                    <button className="search-btn">Търси</button>
+                </form>
+
+                {eventsResult.length > 0 && (
+                    <div className="search-results" ref={searchResultsRef}>
+                        {eventsResult.map((event) => (
+                            <Link to={`/events/${event._id}/details`} className="search-result-item" key={event._id}>
+                                <img src={event.imageUrl} alt={event.title} className="search-result-image" />
+                                <div className="search-result-info">
+                                    <h4>{event.title}</h4>
+                                    <p>
+                                        {event.location} | {event.date}
+                                    </p>
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                )}
             </section>
 
-            <section className="events">
+            <section className="latest-events">
                 <h2>LATEST EVENTS</h2>
                 <div className="event-list">
                     {latestEvents.map((event) => (
@@ -34,7 +91,7 @@ export default function Home() {
                 </div>
             </section>
 
-            <section className="events">
+            <section className="top-events">
                 <h2>TOP EVENTS</h2>
                 <div className="event-list">
                     {topEvents.map((event) => (
